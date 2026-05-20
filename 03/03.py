db8e36ae407bb4ff857a4cb7e0059bf59ed361f9
Giovana Joice <89097348+GiovanaJoice@users.noreply.github.com>
2026-05-20 14:13:24 -0300

    feat: add 03

--- 03/03.py
+++ 03/03.py
@@ -1,2 +1,19 @@
 #Lemos as imagens na ordem invertida: BGR (Azul, Verde e Vermelho). 
 #O Canal 0 é o Azul, o Canal 1 é o Verde e o Canal 2 é o Vermelho.
+
+import numpy as np
+import cv2 as cv
+
+imagem = cv.imread('C:/enviroment/learningOpenCV/03/imgcolorida3.jpg')
+b, g, r = cv.split(imagem) 
+
+cv.imshow('Azul', b)
+cv.imshow('Verde', g)
+cv.imshow('Vermelho', r)
+
+cv.imwrite('C:/enviroment/learningOpenCV/03/filtroazul.jpg', b)
+cv.imwrite('C:/enviroment/learningOpenCV/03/filtroverde.jpg', g )
+cv.imwrite('C:/enviroment/learningOpenCV/03/filtrovermelho.jpg', r)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
