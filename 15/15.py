e385beca4caa32834d52f7d2abf3e27f11acf1de
Giovana Joice <89097348+GiovanaJoice@users.noreply.github.com>
2026-08-03 09:36:19 -0300

    refactor

--- 15/15.py
+++ 15/15.py
@@ -9,12 +9,13 @@ while rodando:
   status, frame = camera.read()
 
   framecinza = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
+  frameCanny = cv.Canny(framecinza, 90, 160)
   if not status or cv.waitKey(1) & 0xFF == ord('x'): #aperte x pra fechar a camera
     rodando = False
     break
 
   cv.imshow('Camera', frame)
+  cv.imshow('Camera Canny', frameCanny)
   cv.imshow('Camera Cinza', framecinza)
 
   cv.imwrite('C:/enviroment/learningOpenCV/15/camera.jpg', framecinza)
@@ -24,8 +25,8 @@ camera.release()
 cv.destroyAllWindows()
 
 cv.imshow('Camera cinza', framecinza)
-frameComFiltroCanny = cv.Canny(framecinza, 100, 168)
-cv.imshow('Camera com Filtro Canny', frameComFiltroCanny)
-cv.imwrite('C:/enviroment/learningOpenCV/15/camera_canny.jpg', frameComFiltroCanny)
+cv.imshow('Camera com Filtro Canny', frameCanny)
+cv.imwrite('C:/enviroment/learningOpenCV/15/camera_canny.jpg', frameCanny)
 cv.waitKey(0)
 cv.destroyAllWindows()
+
