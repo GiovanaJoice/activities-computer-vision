50b602b7b1821ae8741b6d7b435abfdbe5850fef
Giovana Joice <89097348+GiovanaJoice@users.noreply.github.com>
2026-08-12 09:18:25 -0300

    refactor: 20 - 22

--- 22/22.py
+++ 22/22.py
@@ -1,11 +1,9 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
-#import pyautogui
 
-
-imagemOriginal = cv.imread('C:/enviroment/learningOpenCV/22/papagaioDoCongo.webp')
-altura, largura, canal = imagemOriginal.shape
+imagemOriginal = cv.imread('C:/enviroment/learningOpenCV/22/imagemGerada.png')
+imagemCinza = cv.cvtColor(imagemOriginal, cv.COLOR_BGR2GRAY)
+altura, largura = imagemCinza.shape
 print(f'Altura: {altura}, Largura: {largura}')
 
 def selecionarSeed(event, x, y, flags, param):
@@ -15,63 +13,55 @@ def selecionarSeed(event, x, y, flags, param):
         algoritmoDeCrescimentoDeRegiao(seed)
 
 def algoritmoDeCrescimentoDeRegiao(seed):
-   corSemente = imagemOriginal[seed]
-   rangeTolerado = 35 #definindo a variacao de cor q aceitamos
-   regiao = np.zeros((altura, largura), dtype=np.uint8)
-   listaDePixels = [seed] #lista criada começando com a seed sendo passada e ela n pode ser vazia
-   pixelsVisitados = np.zeros((altura, largura), dtype=bool)
-   pixelsVisitados[seed] = True
-   vizinhos = [(-1, 0), (1, 0), (0, -1), (0, 1)] #cima, baixo, esquerda, direita
+    regiao = np.zeros((altura, largura), dtype=np.uint8)
+    listaDePixels = [seed]
+    pixelsVisitados = np.zeros((altura, largura), dtype=bool)
+    pixelsVisitados[seed] = True
     
+    vizinhos = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-   while len(listaDePixels) > 0:
-     yAtual, xAtual = listaDePixels.pop(0)
-     regiao[yAtual, xAtual] = 255 #marca o pixel atual como parte da região
-     for dy, dx in vizinhos:
-        yVizinho = yAtual + dy
-        xVizinho = xAtual + dx
+    while len(listaDePixels) > 0:
+        yAtual, xAtual = listaDePixels.pop(0)
+        regiao[yAtual, xAtual] = 255
+        
+        for dy, dx in vizinhos:
+            yVizinho = yAtual + dy
+            xVizinho = xAtual + dx
 
-        if 0 <= yVizinho < altura and 0 <= xVizinho < largura:
-           if not pixelsVisitados[yVizinho, xVizinho]: #Se for falso, significa que o pixel ainda não foi visitado
-              pixelsVisitados[yVizinho, xVizinho] = True #marca como visitado
-              corVizinho = imagemOriginal[yVizinho, xVizinho] #pegando a cor bgr dos pixels vizinho na imagem original
-              distancia = np.linalg.norm(corVizinho.astype(int) - corSemente.astype(int))
+            if 0 <= yVizinho < altura and 0 <= xVizinho < largura:
+                if not pixelsVisitados[yVizinho, xVizinho]:
+                    pixelsVisitados[yVizinho, xVizinho] = True
+                    if imagemCinza[yVizinho, xVizinho] < 127:
+                        listaDePixels.append((yVizinho, xVizinho))
 
-              if distancia < rangeTolerado:
-                 listaDePixels.append((yVizinho, xVizinho)) #adiciona a coordenada no final da fila
-  
-   #CoDIGO DA Questao 11 reaproveitado
-   contadorX = 0
-   contadorY = 0
-   contadorPixels = 0
+    imagemResultado = np.zeros((altura, largura, 3), dtype=np.uint8)
+    imagemResultado[regiao == 255] = [255, 0, 0]
+    
+    somaX = 0
+    somaY = 0
+    contadorDePixels = 0
+    
+    for i in range(altura):
+        for j in range(largura):
+            if regiao[i, j] == 255:
+                somaX += j
+                somaY += i
+                contadorDePixels += 1
 
-   for i in range(altura): #linha  -> y
-      for j in range(largura): #coluna -> x
-         pixel = regiao[i, j]
-         if pixel == 255: #se o pixel for parte da região crescida
-               contadorX += j
-               contadorY += i
-               contadorPixels += 1
+    if contadorDePixels > 0:
+        xc = somaX // contadorDePixels
+        yc = somaY // contadorDePixels
+        print(f"Centróide calculated: Xc={xc}, Yc={yc}")
+        
+        cv.circle(imagemResultado, (xc, yc), 4, (0, 255, 0), -1)
 
-   if contadorPixels > 0:
-      centroX = contadorX / contadorPixels
-      centroY = contadorY / contadorPixels
-      #achando as coordenadas do centro
-      print('Centro X: ', centroX)
-      print('Centro Y: ', centroY)
-
-      imagemColorida = np.zeros((altura, largura, 3), dtype=np.uint8)
-      imagemColorida[regiao == 255] = [255, 0, 0]  # Azul para a região crescida
-      cv.circle(imagemColorida, (int(centroX), int(centroY)), 5, (0, 255, 0), -1)
-      cv.imwrite('C:/enviroment/learningOpenCV/22/imagemGerada.jpg', imagemColorida)
-      cv.imshow('Imagem com Centro', imagemColorida)
-   else:
-      print('Nenhum pixel  encontrado na imagem.')
+        cv.imshow('Regiao azul com centroide', imagemResultado)
+        cv.imwrite('C:/enviroment/learningOpenCV/22/resultadoQ22.png', imagemResultado)
 
 nomeJanela = 'Clique na imagem para selecionar a seed'
 cv.namedWindow(nomeJanela)
 cv.setMouseCallback(nomeJanela, selecionarSeed)
-cv.imshow(nomeJanela, imagemOriginal)
+cv.imshow(nomeJanela, imagemCinza)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
---
+++ 24/24.py
@@ -0,0 +1,77 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+#import pyautogui
+
+
+imagemOriginal = cv.imread('C:/enviroment/learningOpenCV/24/papagaioDoCongo.webp')
+altura, largura, canal = imagemOriginal.shape
+print(f'Altura: {altura}, Largura: {largura}')
+
+def selecionarSeed(event, x, y, flags, param):
+    if event == cv.EVENT_LBUTTONDOWN:
+        print(f'Seed selecionada: ({x}, {y})')
+        seed = (y, x)
+        algoritmoDeCrescimentoDeRegiao(seed)
+
+def algoritmoDeCrescimentoDeRegiao(seed):
+   corSemente = imagemOriginal[seed]
+   rangeTolerado = 35 #definindo a variacao de cor q aceitamos
+   regiao = np.zeros((altura, largura), dtype=np.uint8)
+   listaDePixels = [seed] #lista criada começando com a seed sendo passada e ela n pode ser vazia
+   pixelsVisitados = np.zeros((altura, largura), dtype=bool)
+   pixelsVisitados[seed] = True
+   vizinhos = [(-1, 0), (1, 0), (0, -1), (0, 1)] #cima, baixo, esquerda, direita
+    
+
+   while len(listaDePixels) > 0:
+     yAtual, xAtual = listaDePixels.pop(0)
+     regiao[yAtual, xAtual] = 255 #marca o pixel atual como parte da região
+     for dy, dx in vizinhos:
+        yVizinho = yAtual + dy
+        xVizinho = xAtual + dx
+
+        if 0 <= yVizinho < altura and 0 <= xVizinho < largura:
+           if not pixelsVisitados[yVizinho, xVizinho]: #Se for falso, significa que o pixel ainda não foi visitado
+              pixelsVisitados[yVizinho, xVizinho] = True #marca como visitado
+              corVizinho = imagemOriginal[yVizinho, xVizinho] #pegando a cor bgr dos pixels vizinho na imagem original
+              distancia = np.linalg.norm(corVizinho.astype(int) - corSemente.astype(int))
+
+              if distancia < rangeTolerado:
+                 listaDePixels.append((yVizinho, xVizinho)) #adiciona a coordenada no final da fila
+  
+   #CoDIGO DA Questao 11 reaproveitado
+   contadorX = 0
+   contadorY = 0
+   contadorPixels = 0
+
+   for i in range(altura): #linha  -> y
+      for j in range(largura): #coluna -> x
+         pixel = regiao[i, j]
+         if pixel == 255: #se o pixel for parte da região crescida
+               contadorX += j
+               contadorY += i
+               contadorPixels += 1
+
+   if contadorPixels > 0:
+      centroX = contadorX / contadorPixels
+      centroY = contadorY / contadorPixels
+      #achando as coordenadas do centro
+      print('Centro X: ', centroX)
+      print('Centro Y: ', centroY)
+
+      imagemColorida = np.zeros((altura, largura, 3), dtype=np.uint8)
+      imagemColorida[regiao == 255] = [255, 0, 0]  # Azul para a região crescida
+      cv.circle(imagemColorida, (int(centroX), int(centroY)), 5, (0, 255, 0), -1)
+      cv.imwrite('C:/enviroment/learningOpenCV/22/imagemGerada.jpg', imagemColorida)
+      cv.imshow('Imagem com Centro', imagemColorida)
+   else:
+      print('Nenhum pixel  encontrado na imagem.')
+
+nomeJanela = 'Clique na imagem para selecionar a seed'
+cv.namedWindow(nomeJanela)
+cv.setMouseCallback(nomeJanela, selecionarSeed)
+cv.imshow(nomeJanela, imagemOriginal)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
